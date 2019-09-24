b7cc2129469ec33d01118677fcb2b753c43f77a0
Gustavo <gussj@users.noreply.github.com>
2019-09-23 21:59:18 -0300

    Update csand.sh

--- csand.sh
+++ csand.sh
@@ -185,12 +185,14 @@ if [ "$1" = "vmcloack" ]
 	vmcloak clone win7x64base win7x64cuckoo
 	vmcloak install win7x64cuckoo adobepdf pillow dotnet java flash vcredist vcredist.version=2015u3 wallpaper ie11
 	vmcloak snapshot --count 4 win7x64cuckoo_ 192.168.56.101
+	// this should be moved to root user
 	$sudo_cmd sysctl -w net.ipv4.conf.vboxnet0.forwarding=1
 	$sudo_cmd sysctl -w net.ipv4.conf.eth0.forwarding=1
 	$sudo_cmd iptables -t nat -A POSTROUTING -o eth0 -s 192.168.56.0/24 -j MASQUERADE
 	$sudo_cmd iptables -P FORWARD DROP
 	$sudo_cmd iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 	$sudo_cmd iptables -A FORWARD -s 192.168.56.0/24 -j ACCEPT
+	// end of move
 	cuckoo init
 	cuckoo web --host 127.0.0.1 --port 8080
 	supervisord -c /home/cuckoo/.cuckoo/supervisord.conf

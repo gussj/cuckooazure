1d1085ba51b1dde8cbec36dab38289c7e0474570
Gustavo <gussj@users.noreply.github.com>
2020-01-16 00:48:23 -0300

    Update csand.sh
    Several fixes

--- csand.sh
+++ csand.sh
@@ -61,47 +61,48 @@ if [ "$1" = "prereq" ]
 	fi
 	echo "Installing pre-reqs"
 	#$(sudo add-apt-repository universe)
-	$sudo_cmd add-apt-repository universe
-	$sudo_cmd add-apt-repository multiverse
-	$sudo_cmd echo "deb https://download.virtualbox.org/virtualbox/debian disco contrib" | sudo tee -a /etc/apt/sources.list
-	$sudo_cmd wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-	$sudo_cmd wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-	$sudo_cmd apt-get update
-	$sudo_cmd apt-get upgrade --allow-downgrades --allow-remove-essential --allow-change-held-packages
-	$sudo_cmd apt-get install iptables-persistent git libffi-dev libjpeg8-dev zlib1g-dev genisoimage supervisor uwsgi uwsgi-plugin-python nginx build-essential unzip python-django python python-dev python-pip python-pil python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile python-chardet tcpdump apparmor-utils libjpeg-dev python-virtualenv python3-virtualenv virtualenv swig libpq-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev virtualbox-5.2 -y
-	$sudo_cmd adduser --disabled-password --gecos "" cuckoo
-	$sudo_cmd groupadd pcap
-	$sudo_cmd usermod -a -G pcap cuckoo
-	$sudo_cmd chgrp pcap /usr/sbin/tcpdump
-	$sudo_cmd setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+	sudo add-apt-repository universe
+	sudo add-apt-repository multiverse
+	sudo echo "deb https://download.virtualbox.org/virtualbox/debian disco contrib" | sudo tee -a /etc/apt/sources.list
+	sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+	sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get upgrade --allow-downgrades --allow-remove-essential --allow-change-held-packages
+	sudo apt-get install iptables-persistent git libffi-dev libjpeg8-dev zlib1g-dev genisoimage supervisor uwsgi uwsgi-plugin-python nginx build-essential unzip python-django python python-dev python-pip python-pil python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile python-chardet tcpdump apparmor-utils libjpeg-dev python-virtualenv python3-virtualenv virtualenv swig libpq-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev virtualbox-5.2 -y
+	sudo adduser --disabled-password --gecos "" cuckoo
+	sudo groupadd pcap
+	sudo usermod -a -G pcap cuckoo
+	sudo chgrp pcap /usr/sbin/tcpdump
+	sudo aa-disable /usr/sbin/tcpdump
+	sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 	cd
-	$sudo_cmd mkdir /home/"$CURRENTUSER"/csand
+	sudo mkdir /home/"$CURRENTUSER"/csand
 	cd /home/"$CURRENTUSER"/csand
-	$sudo_cmd mkdir files
+	sudo mkdir files
 	cd files
-	$sudo_cmd wget https://download.virtualbox.org/virtualbox/5.2.34/Oracle_VM_VirtualBox_Extension_Pack-5.2.34.vbox-extpack
-	$sudo_cmd VBoxManage extpack install https://download.virtualbox.org/virtualbox/5.2.34/Oracle_VM_VirtualBox_Extension_Pack-5.2.34.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
-	$sudo_cmd usermod -a -G vboxusers cuckoo
-	$sudo_cmd wget https://github.com/VirusTotal/yara/archive/v3.10.0.tar.gz -O yara-3.10.0.tar.gz
-	$sudo_cmd tar -zxf yara-3.10.0.tar.gz
+	sudo wget https://download.virtualbox.org/virtualbox/5.2.34/Oracle_VM_VirtualBox_Extension_Pack-5.2.34.vbox-extpack
+	sudo VBoxManage extpack install https://download.virtualbox.org/virtualbox/5.2.34/Oracle_VM_VirtualBox_Extension_Pack-5.2.34.vbox-extpack --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
+	sudo usermod -a -G vboxusers cuckoo
+	sudo wget https://github.com/VirusTotal/yara/archive/v3.10.0.tar.gz -O yara-3.10.0.tar.gz
+	sudo tar -zxf yara-3.10.0.tar.gz
 	cd yara-3.10.0
-	$sudo_cmd ./bootstrap.sh
-	$sudo_cmd ./configure -with-crypto -enable-cuckoo -enable-magic
-	$sudo_cmd make
-	$sudo_cmd make install
-	$sudo_cmd ln -s /usr/local/lib/libyara.so.3 /usr/lib/libyara.so.3
-	$sudo_cmd wget https://github.com/VirusTotal/yara-python/archive/v3.10.0.tar.gz -O yara-python.tar.gz
-	$sudo_cmd tar -zxf yara-python.tar.gz
+	sudo ./bootstrap.sh
+	sudo ./configure -with-crypto -enable-cuckoo -enable-magic
+	sudo make
+	sudo make install
+	sudo ln -s /usr/local/lib/libyara.so.3 /usr/lib/libyara.so.3
+	sudo wget https://github.com/VirusTotal/yara-python/archive/v3.10.0.tar.gz -O yara-python.tar.gz
+	sudo tar -zxf yara-python.tar.gz
 	cd yara-python-3.10.0
-	$sudo_cmd python setup.py build
-	$sudo_cmd python setup.py install
+	sudo python setup.py build
+	sudo python setup.py install
 	cd /home/"$CURRENTUSER"/csand/files/
-	$sudo_cmd wget https://github.com/ssdeep-project/ssdeep/releases/download/release-2.14.1/ssdeep-2.14.1.tar.gz -O ssdeep-2.14.1.tar.gz
-	$sudo_cmd tar -zxf ssdeep-2.14.1.tar.gz
+	sudo wget https://github.com/ssdeep-project/ssdeep/releases/download/release-2.14.1/ssdeep-2.14.1.tar.gz -O ssdeep-2.14.1.tar.gz
+	sudo tar -zxf ssdeep-2.14.1.tar.gz
 	cd ssdeep-2.14.1
-	$sudo_cmd ./configure
-	$sudo_cmd make
-	$sudo_cmd make install
+	sudo ./configure
+	sudo make
+	sudo make install
 	pip install pydeep
 	pip install openpyxl
 	pip install ujson
@@ -110,16 +111,16 @@ if [ "$1" = "prereq" ]
 	pip install pytz
 	pip install jsonschema
 	cd /home/"$CURRENTUSER"/csand/files/
-	$sudo_cmd git clone https://github.com/volatilityfoundation/volatility.git
+	sudo git clone https://github.com/volatilityfoundation/volatility.git
 	cd volatility
-	$sudo_cmd python setup.py build
-	$sudo_cmd python setup.py install
+	sudo python setup.py build
+	sudo python setup.py install
 	cd /home/"$CURRENTUSER"/csand/files/
-	$sudo_cmd wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip
-	$sudo_cmd unzip packer_1.4.3_linux_amd64.zip
-	$sudo_cmd mv packer /usr/local/bin
-	$sudo_cmd wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
-	$sudo_cmd dpkg -i vagrant_2.2.5_x86_64.deb
+	sudo wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip
+	sudo unzip packer_1.4.3_linux_amd64.zip
+	sudo mv packer /usr/local/bin
+	sudo wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
+	sudo dpkg -i vagrant_2.2.5_x86_64.deb
     echo "Finish installing pre-reqs"
 exit
 fi
@@ -164,25 +165,25 @@ if [ "$1" = "vmcloack" ]
 	if [ -d "$DIR" ]; then
 	echo "Initiating VMCloak and Cuckoo Installation"
 	cd /home/"$CURRENTUSER"/csand/
-	$sudo_cmd apt-get update
-	$sudo_cmd apt-get -y install python virtualenv python-pip python-dev build-essential
-	$sudo_cmd apt-get -y install postgresql postgresql-contrib
-	$sudo_cmd wget https://cuckoo.sh/win7ultimate.iso
-	$sudo_cmd mkdir /mnt/win7
-	$sudo_cmd mount -o ro,loop win7ultimate.iso /mnt/win7
-	$sudo_cmd apt-get -y install build-essential libssl-dev libffi-dev python-dev genisoimage mongodb supervisord
-	$sudo_cmd apt-get -y install zlib1g-dev libjpeg-dev
-	$sudo_cmd apt-get -y install python-pip python-virtualenv python-setuptools swig
+	sudo apt-get update
+	sudo apt-get -y install python virtualenv python-pip python-dev build-essential
+	sudo apt-get -y install postgresql postgresql-contrib
+	sudo wget https://cuckoo.sh/win7ultimate.iso
+	sudo mkdir /mnt/win7
+	sudo mount -o ro,loop win7ultimate.iso /mnt/win7
+	sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev genisoimage mongodb supervisord python3-all-dev
+	sudo apt-get -y install zlib1g-dev libjpeg-dev
+	sudo apt-get -y install python-pip python-virtualenv python-setuptools swig
 	pip install --upgrade pip
-	pip install pillow
-	pip install --upgrade pillow
+	python -m pip install pillow
+	python -m pip install --upgrade pillow
 	sudo -u postgres psql
 	CREATE DATABASE cuckoo;
 	CREATE USER cuckoo WITH ENCRYPTED PASSWORD 'password';
 	GRANT ALL PRIVILEGES ON DATABASE cuckoo TO cuckoo;
 	\q
 	#edit conf/cuckoo connection = postgresql://cuckoo:password@localhost/cuckoo
-	$sudo_cmd su cuckoo
+	sudo su cuckoo
 	virtualenv ~/cuckoo
 	. ~/cuckoo/bin/activate
 	pip install -U cuckoo vmcloak uwsgi psycopg2
@@ -191,14 +192,14 @@ if [ "$1" = "vmcloack" ]
 	vmcloak init --verbose --win7x64 win7x64base --cpus 2 --ramsize 2048
 	vmcloak clone win7x64base win7x64cuckoo
 	vmcloak install win7x64cuckoo adobepdf pillow dotnet java flash vcredist vcredist.version=2015u3 wallpaper ie11
-	vmcloak snapshot --count 4 win7x64cuckoo_ 192.168.56.101
+	vmcloak snapshot --count 4 win7x64cuckoo 192.168.56.101
 	# this should be moved to root user
-	$sudo_cmd sysctl -w net.ipv4.conf.vboxnet0.forwarding=1
-	$sudo_cmd sysctl -w net.ipv4.conf.eth0.forwarding=1
-	$sudo_cmd iptables -t nat -A POSTROUTING -o eth0 -s 192.168.56.0/24 -j MASQUERADE
-	$sudo_cmd iptables -P FORWARD DROP
-	$sudo_cmd iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-	$sudo_cmd iptables -A FORWARD -s 192.168.56.0/24 -j ACCEPT
+	sudo sysctl -w net.ipv4.conf.vboxnet0.forwarding=1
+	sudo sysctl -w net.ipv4.conf.eth0.forwarding=1
+	sudo iptables -t nat -A POSTROUTING -o eth0 -s 192.168.56.0/24 -j MASQUERADE
+	sudo iptables -P FORWARD DROP
+	sudo iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+	sudo iptables -A FORWARD -s 192.168.56.0/24 -j ACCEPT
 	# end of move
 	cuckoo init
 	# need to modify the cuckoo files with no machines. /conf/virtualbox.conf machines =
@@ -235,19 +236,19 @@ if [ "$1" = "vmcloack" ]
 	else
 	mkdir /home/"$CURRENTUSER"/csand
 	cd /home/"$CURRENTUSER"/csand/
-	$sudo_cmd apt-get update
-	$sudo_cmd apt-get -y install python virtualenv python-pip python-dev build-essential
-	$sudo_cmd apt-get -y install postgresql postgresql-contrib
-	$sudo_cmd wget https://cuckoo.sh/win7ultimate.iso
-	$sudo_cmd mkdir /mnt/win7
-	$sudo_cmd mount -o ro,loop win7ultimate.iso /mnt/win7
-	$sudo_cmd apt-get -y install build-essential libssl-dev libffi-dev python-dev genisoimage mongodb supervisord
-	$sudo_cmd apt-get -y install zlib1g-dev libjpeg-dev
-	$sudo_cmd apt-get -y install python-pip python-virtualenv python-setuptools swig
+	sudo apt-get update
+	sudo apt-get -y install python virtualenv python-pip python-dev build-essential
+	sudo apt-get -y install postgresql postgresql-contrib
+	sudo wget https://cuckoo.sh/win7ultimate.iso
+	sudo mkdir /mnt/win7
+	sudo mount -o ro,loop win7ultimate.iso /mnt/win7
+	sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev genisoimage mongodb supervisord
+	sudo apt-get -y install zlib1g-dev libjpeg-dev
+	sudo apt-get -y install python-pip python-virtualenv python-setuptools swig
 	pip install --upgrade pip
 	pip install pillow
 	pip install --upgrade pillow
-	$sudo_cmd su cuckoo
+	sudo su cuckoo
 	virtualenv ~/cuckoo
 	. ~/cuckoo/bin/activate
 	pip install -U cuckoo
@@ -257,12 +258,12 @@ if [ "$1" = "vmcloack" ]
 	vmcloak clone win7x64base win7x64cuckoo
 	vmcloak install win7x64cuckoo adobepdf pillow dotnet java flash vcredist vcredist.version=2015u3 wallpaper ie11
 	vmcloak snapshot --count 4 win7x64cuckoo_ 192.168.56.101
-	$sudo_cmd sysctl -w net.ipv4.conf.vboxnet0.forwarding=1
-	$sudo_cmd sysctl -w net.ipv4.conf.eth0.forwarding=1
-	$sudo_cmd iptables -t nat -A POSTROUTING -o eth0 -s 192.168.56.0/24 -j MASQUERADE
-	$sudo_cmd iptables -P FORWARD DROP
-	$sudo_cmd iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-	$sudo_cmd iptables -A FORWARD -s 192.168.56.0/24 -j ACCEPT
+	sudo sysctl -w net.ipv4.conf.vboxnet0.forwarding=1
+	sudo sysctl -w net.ipv4.conf.eth0.forwarding=1
+	sudo iptables -t nat -A POSTROUTING -o eth0 -s 192.168.56.0/24 -j MASQUERADE
+	sudo iptables -P FORWARD DROP
+	sudo iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+	sudo iptables -A FORWARD -s 192.168.56.0/24 -j ACCEPT
 	cuckoo init
 	cuckoo web --host 127.0.0.1 --port 8080
 	supervisord -c /home/cuckoo/.cuckoo/supervisord.conf
@@ -270,3 +271,8 @@ if [ "$1" = "vmcloack" ]
 	fi
 exit
 fi
+
+#in restart use this:
+#. ~/cuckoo/bin/activate
+#vmcloak-vboxnet0
+#supervisord -c /home/cuckoo/.cuckoo/supervisord.conf

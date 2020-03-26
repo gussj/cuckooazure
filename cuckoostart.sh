9ad0f04079db214f0885ba8c057b7e4d95d8124c
Gustavo <gussj@users.noreply.github.com>
2020-03-26 19:14:35 -0300

    Update cuckoostart.sh

--- cuckoostart.sh
+++ cuckoostart.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo su cuckoo
-. ~/cuckoo/bin/activate
-vmcloak-vboxnet0
-supervisord -c /home/cuckoo/.cuckoo/supervisord.conf
+su cuckoo -c '. ~/cuckoo/bin/activate && vmcloak-vboxnet0 && supervisord -c /home/cuckoo/.cuckoo/supervisord.conf'
+exit
